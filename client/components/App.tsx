2a28c220c150147ce0aeadb847085b577f292c6b
Tem <tem.pearse01@gmail.com>
2023-06-07 11:56:39 +1200

    2 done

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from 'react'
+import { Widget } from '../../models/Widget'
+
 function App() {
+  const [widgets, setWidgets] = useState([] as Widget[])
+
+  useEffect(() => {
+    console.log('using the effect')
+
+    //   fetchWidgets()
+    //     .then((fetchedWidgets) => {
+    //       setWidgets(fetchedWidgets)
+    //     })
+    //     .catch((err) => {
+    //       if (err instanceof Error) {
+    //         console.error(err.message)
+    //       }
+    //     })
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
--- models/Widget.ts
+++ models/Widget.ts
@@ -1,10 +1,12 @@
+
+//front
 export interface NewWidget {
   name: string
   price: number
   mfg: string
   inStock: number
 }
-
+//back
 export interface Widget {
   id: number
   name: string
