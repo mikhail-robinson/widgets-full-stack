580b0694ba98aa2f7917bffb1e5494fbcf9c7f66
Andrew <andrewjtrotter@gmail.com>
2023-06-07 11:57:20 +1200

    initial commit. Rendered widget data to App

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import request from 'superagent'
+import { Widget } from '../models/Widget'
 
 const widgetUrl = '/api/v1/widgets/'
 
-export function getWidgets(): void {}
+export async function getWidgets() {
+  const res = await request.get(widgetUrl)    
+  return res.body as Widget[]
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react"
+import { Widget } from "../../models/Widget"
+import { getWidgets } from "../apiClient"
+
+const initialWidgetsArr: Widget[] = [
+
+]
+
 function App() {
+  const [widgets, setWidgets] = useState(initialWidgetsArr as Widget[])
+
+  useEffect(() => {
+    getWidgets()
+    .then((fetchedWidgets) => {
+      setWidgets(fetchedWidgets)
+    }).catch((error) => {
+      if (error instanceof Error) {
+        console.error(`Something went wrong when fetching widgets`);        
+      }
+    })    
+  }, [])
   return (
+    <>
     <div>
       <h1>Widgets for the win!</h1>
+      <ul>
+        {widgets.map(widget => {
+          return(
+          <li key={widget.id}>{widget.name}</li>)
+        })}
+      </ul>
     </div>
+    </>
   )
 }
 
