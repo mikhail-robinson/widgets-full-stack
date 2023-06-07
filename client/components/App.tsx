0027d01f1f4cd3a32c70cead850ab07e55380004
Maia <tarapipipim@gmail.com>
2023-06-07 12:08:43 +1200

    adds useEffect to App component set up

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react'
+import { Widget } from '../../models/Widget'
+
 function App() {
+  // add use state to store widgets and import Widget type
+  const [widgets, setWidgets] = useState([] as Widget[])
+
+  // add use effect which accepts a function
+  useEffect(() => {
+    //API function will go here
+    //. then to promise + pass a paramater which is relivent to the function
+    // call setWidgets to change state
+    // . catch any errors
+    console.log('using the effect')
+  }, [])
   return (
     <div>
       <h1>Widgets for the win!</h1>
