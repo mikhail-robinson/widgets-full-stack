3f5bca1d767efab7a0d45a665854d62ce94b5c4a
Jaelyn <leejay1020@gmail.com>
2023-06-07 14:30:09 +1200

    connect server API to the client API and log out the widgets on console

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,25 @@
-import { useEffect, useState } from "react"
-import { Widget } from "../../models/Widget"
+import { useEffect, useState } from 'react'
+import { Widget } from '../../models/Widget'
+import { getWidgets } from '../apiClient'
 
 function App() {
-  const [ widgets, setWeidgets ] = useState([] as Widget)
-  
-  useEffect (() => {
-    console.log('using the effect')
-  },[])
+  const [widgets, setWeidgets] = useState([] as Widget[])
+
+  useEffect(() => {
+    getWidgets().then(fetchedwidgets => {
+      console.log("result of getWidgets: ", fetchedwidgets)
+    }).catch((error) => {
+      if (error instanceof Error) {
+        console.error("Ops, something went wrong when fetching data.")
+      }
+    })
+  }
+    , [])
 
   return (
     <div>
       <h1>Widgets for the win!</h1>
+      
     </div>
   )
 }
