78225146f6e9a130d546eb74c0c671e02cfc7af7
Jaelyn <leejay1020@gmail.com>
2023-06-07 17:02:44 +1200

    make the adding function working

--- server/routes/widgets.ts
+++ server/routes/widgets.ts
@@ -1,16 +1,31 @@
 import express from 'express'
-import { getWidgets } from '../db/db'
+import * as db from '../db/db'
+import { widgetSchema } from '../../models/Widget'
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  getWidgets()
+  db.getWidgets()
     .then((widgets) => {
       res.json(widgets)
     })
     .catch((err) => {
       res.status(500).send(err.message)
     })
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const input = req.body
+    // const widgetData = widgetSchema.parse(input)
+    const widgetData =  
+    const ids = await db.addWidgets(widgetData)
+    res.json({ id: ids[0] })
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message })
+    }
+  }
 })
 
 export default router
