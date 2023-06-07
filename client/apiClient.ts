dfdd0152a5509a589ce7e2fec40be230d28c6362
Adam <adamcrowley@gmail.com>
2023-06-07 16:54:52 +1200

    more stuff

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -3,4 +3,9 @@ import request from 'superagent'
 
 const widgetUrl = '/api/v1/widgets/'
 
-export function getWidgets(): void {}
+export async function getWidgets() {
+  //make a GET request to '/api/v1/widgets/'
+  const res = await request.get(widgetUrl)
+  //return response body
+  return res.body
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,17 +1,29 @@
 import { useEffect, useState } from 'react'
 import { Widget } from '../../models/Widget'
+import { getWidgets } from '../apiClient'
 
 function App() {
   const [widgets, setWidgets] = useState([] as Widget[])
 
   useEffect(() => {
-    // loadLanguages()
-
+    loadWidgets()
     console.log('using the effect')
   }, [])
 
+  function loadWidgets() {
+    //get the widgets from the api
+    getWidgets()
+      .then((data) => console.log(data))
+      .catch((err) => {
+        if (err instanceof Error) {
+          console.error(err.message)
+        }
+      })
+  }
+
   return (
     <div>
+      <p>{}</p>
       <h1>Widgets for the win!</h1>
     </div>
   )
