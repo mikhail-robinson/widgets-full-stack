f2069df3977c72e707a5a458456c682dae32ad5d
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-07 13:04:04 +1200

    update UI to consume widget names

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react'
 import { Widget } from '../../models/Widget'
 import { getWidgets } from '../apiClient'
+import * as Models from '../../models/Widget'
 function App() {
-  const [widgets, setWidgets] = useState([] as Widget[])
+  const [widgets, setWidgets] = useState([] as Models.Widget[])
 
   useEffect(() => {
+    loadWidgets()
+  }, [])
+
+  function loadWidgets() {
     getWidgets()
       .then((fetchedWidgets) => {
         setWidgets(fetchedWidgets)
@@ -14,13 +19,18 @@ function App() {
           console.error(err.message)
         }
       })
-  }, [])
+  }
 
-  console.log(widgets)
   return (
-    <div>
-      <h1>Widgets for the win!</h1>
-    </div>
+    <>
+      <div>
+        <h1>Widgets for the win!</h1>
+
+        {widgets.map((widget) => (
+          <p key={widget.id}>{widget.name}</p>
+        ))}
+      </div>
+    </>
   )
 }
 
