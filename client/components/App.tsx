2d4b48840dd4a29e40413d29c627da4e3f0c3d0d
Daniel <danielgrabczewski@gmail.com>
2023-06-07 12:43:52 +1200

    complete up to 5

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react'
+import * as Models from '../../models/Widget'
+import { getWidgets } from '../apiClient'
+import Widget from './Widget'
+
+
 function App() {
+  const [widgets, setWidgets] = useState([] as Models.Widget[])
+
+  useEffect(() => {
+    getWidgets().then((res) => setWidgets(res)).catch((err) => 
+    { 
+      if (err instanceof Error) 
+      console.error(err.message)})
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
+      {
+        widgets.map((widget) => (
+            <Widget key = {widget.name} id={widget.id} name={widget.name} price = {widget.price} mfg = {widget.mfg} inStock = {widget.inStock}/>
+        ))
+      }
     </div>
   )
 }
---
+++ client/components/Widget.tsx
@@ -0,0 +1,19 @@
+import * as Models from '../../models/Widget'
+
+function Widget(props : Models.Widget ) {
+  const {id, name, price, mfg, inStock} = props
+
+  console.log(name.split(' ')[0].toLowerCase())
+  return (
+    <>
+      <div style = {{backgroundColor : name.split(' ')[0] || 'lightgray'}}>
+        <h3>{name}</h3>
+        <p>${price}</p>
+        <p>{mfg}</p>
+        <p>In stock: {inStock}</p>
+      </div>
+    </>
+  )
+}
+
+export default Widget
