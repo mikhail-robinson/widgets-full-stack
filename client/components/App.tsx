64af77ef9a7c58556440ef0917511c6a0b60eee8
Jaelyn <leejay1020@gmail.com>
2023-06-07 14:55:55 +1200

    display widget data on the page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,25 +1,35 @@
 import { useEffect, useState } from 'react'
-import { Widget } from '../../models/Widget'
+import * as Modules from '../../models/Widget'
 import { getWidgets } from '../apiClient'
 
 function App() {
-  const [widgets, setWeidgets] = useState([] as Widget[])
+  const [widgets, setWeidgets] = useState([] as Modules.Widget[])
 
   useEffect(() => {
-    getWidgets().then(fetchedwidgets => {
-      console.log("result of getWidgets: ", fetchedwidgets)
-    }).catch((error) => {
-      if (error instanceof Error) {
-        console.error("Ops, something went wrong when fetching data.")
-      }
-    })
-  }
-    , [])
+    getWidgets()
+      .then((fetchedwidgets) => {
+        setWeidgets(fetchedwidgets)
+      })
+      .catch((error) => {
+        if (error instanceof Error) {
+          console.error('Ops, something went wrong when fetching data.')
+        }
+      })
+  }, [])
 
   return (
     <div>
       <h1>Widgets for the win!</h1>
-      
+      <div>
+        {widgets.map((widget) => (
+          <>
+            <li key={widget.id}>
+              Widget Name: {widget.name} / Widget Price: {widget.price} / In
+              Stock: {widget.inStock}
+            </li>
+          </>
+        ))}
+      </div>
     </div>
   )
 }
