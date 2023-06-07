0831f0fc3cdf8f8e97360fffe1c59664df27b9cf
Sarah Zhong <sarahchungwu@gmail.com>
2023-06-07 16:47:41 +1200

    updating the UI  and fixed the useEffect

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,13 +3,31 @@ import { Widget } from '../../models/Widget'
 import { getWidgets } from '../apiClient'
 function App() {
   const [widgets, setWidgets] = useState([] as Widget[])
-  useEffect(() => {
-    getWidgets().then((widget) => setWidgets(widget)), []
-  })
 
+  useEffect(() => {
+    console.log('render')
+    getWidgets()
+      .then((widget) => setWidgets(widget))
+      .catch((err) => {
+        if (err instanceof Error) {
+          console.error(err.message)
+        }
+      })
+  }, [])
+  console.log(widgets)
   return (
     <div>
       <h1>Widgets for the win!</h1>
+      <ul>
+        {widgets.map((widget) => (
+          <li key={widget.id}>
+            <p>{widget.name}</p>
+            <p>{widget.price}</p>
+            <p>{widget.mfg}</p>
+            <p>{widget.inStock}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
