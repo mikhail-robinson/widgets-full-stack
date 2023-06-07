3e29afb26b6ee8995fb92d103ca7434e294fe20c
Rigelle <rigellecruz@gmail.com>
2023-06-07 12:00:00 +1200

    initial commit

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import request from 'superagent'
+import { Widget } from '../models/Widget'
 
 const widgetUrl = '/api/v1/widgets/'
 
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,19 @@
+import { useState } from 'react'
+import { getWidgets } from '../apiClient'
+
 function App() {
+  const [widgets, setWidgets] = useState([] as Widget[])
+
+  useEffect(() => {
+    getWidgets()
+    .then((widgets) => {
+      setWidgets(widgets)
+    })
+    .catch((error) => {
+      console.error(err.message)
+    })
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
