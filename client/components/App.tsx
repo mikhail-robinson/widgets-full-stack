20dc5f218294b7d846fad8a7d54fbcc8840acbed
Jaelyn <leejay1020@gmail.com>
2023-06-07 12:32:50 +1200

    set up the <App> component with useState and useEffect

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react"
+import { Widget } from "../../models/Widget"
+
 function App() {
+  const [ widgets, setWeidgets ] = useState([] as Widget)
+  
+  useEffect (() => {
+    console.log('using the effect')
+  },[])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
