e5c546fac806105202b4da5aa63106a6737714c1
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-10 14:32:57 +1200

    start route to submit page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from 'react'
 import { getWidgets } from '../apiClient'
 import * as Models from '../../models/Widget'
 import AddWidgetForm from './AddWidget'
+import SubmitPage from './SubmitPage'
 import './main.css'
+import { BrowserRouter as Router, Route, Link, Outlet } from 'react-router-dom'
+
 function App() {
   const [widgets, setWidgets] = useState([] as Models.Widget[])
 
@@ -23,19 +26,26 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Widgets for the win!</h1>
-      <AddWidgetForm loadWidgets={loadWidgets} />
+    <Router>
+      <Route path="/submit">
+        <SubmitPage />
+      </Route>
 
-      {widgets.map((widget) => (
-        <div className="widget" key={widget.id}>
-          <p className="widget-name">Name: {widget.name}</p>
-          <p className="widget-price">Price: ${widget.price}</p>
-          <p className="widget-mfg">MFG: {widget.mfg}</p>
-          <p className="widget-stock">Stock: {widget.inStock}</p>
-        </div>
-      ))}
-    </div>
+      <div className="container">
+        <h1>Widgets for the win!</h1>
+        <AddWidgetForm loadWidgets={loadWidgets} />
+
+        {widgets.map((widget) => (
+          <div className="widget" key={widget.id}>
+            <p className="widget-name">Name: {widget.name}</p>
+            <p className="widget-price">Price: ${widget.price}</p>
+            <p className="widget-mfg">MFG: {widget.mfg}</p>
+            <p className="widget-stock">Stock: {widget.inStock}</p>
+          </div>
+        ))}
+      </div>
+      <Outlet />
+    </Router>
   )
 }
 
---
+++ client/components/SubmitPage.tsx
@@ -0,0 +1,10 @@
+function SubmitPage() {
+  return (
+    <div>
+      <h2>Submit Page</h2>
+      {/* Add the content and form for submitting */}
+    </div>
+  )
+}
+
+export default SubmitPage
