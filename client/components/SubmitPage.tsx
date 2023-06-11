83fc56a14d8918466bfadab746e19ad1c5b58f02
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-11 14:08:25 +1200

    add gif to submit page

--- client/components/SubmitPage.tsx
+++ client/components/SubmitPage.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom'
-import AddWidgetForm from './AddWidget'
 
 interface Props {
   loadWidgets: () => void
@@ -12,6 +11,12 @@ function SubmitPage(props: Form) {
   return (
     <div>
       <h2>You have successfully submitted a new widget! 🥳🥳🥳</h2>
+
+      <img
+        className="party-gif"
+        src="https://media0.giphy.com/media/9rMvwuIpMBKU0/giphy.gif?cid=ecf05e476gxa41puma2ggc5zgqqssv2ov4mwwsu6a2zbl1r1&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+        alt="party-gif"
+      />
       <Link to={'/'} className="form-button" onClick={props.toggleForm}>
         Add another Widget
       </Link>
