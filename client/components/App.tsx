e050cac1e3266814ee55c2f90114d15fb8da1ef6
Quinn Gibson <quinngiibson@gmail.com>
2023-06-12 13:46:39 +1200

    update UI to consume data

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react'
+import * as Models from '../../models/Widget'
+import { getWidgets } from '../apiClient'
+
 function App() {
+  const [widgets, setWidgets] = useState([] as Models.Widget[])
+
+  useEffect(() => {
+    getWidgets()
+      .then((widgets) => {
+        setWidgets(widgets)
+      })
+      .catch((err) => {
+        if (err instanceof Error) {
+          console.error(err.message)
+        }
+      })
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
+      <ul>
+        {widgets.map((widget) => (
+          <li key={widget.id}>
+            <h3>
+              {widget.name}, {widget.mfg} - {widget.price}
+            </h3>
+            <p>In Stock: {widget.inStock}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
